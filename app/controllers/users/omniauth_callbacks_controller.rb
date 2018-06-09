fbdbc080cc9a0ad90c7d4a7cfbf1270f988c7054
zgfif <zihzag@gmail.com>
2018-06-09 11:57:33 +0300

    set omniauth-google

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,17 +11,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
       end
   end
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
-
-    # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data['name'],
-    #        email: data['email'],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
-    user
-emailnd
 end
