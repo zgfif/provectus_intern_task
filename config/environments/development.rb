02637e0cd7908e489909d13b86323d1ad5589354
zgfif <zihzag@gmail.com>
2018-06-09 20:19:30 +0300

    set delivery of confirmation e-mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,8 +53,23 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   #gem letter opener below
   # config.action_mailer.delivery_method = :letter_opener
   # config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] } 
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = true  
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {  
+          address: "smtp.gmail.com",
+          port: 587,
+          domain: "gmail.com",
+          authentication: "plain",
+          enable_starttls_auto: true,      
+          user_name: ENV["SENDMAIL_USERNAME"],    
+          password: ENV["SENDMAIL_PASSWORD"],
+        }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,21 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  # config.active_record.dump_schema_after_migration = false
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] } 
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = true  
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {  
+          address: "smtp.gmail.com",
+          port: 587,
+          domain: "gmail.com",
+          authentication: "plain",
+          enable_starttls_auto: true,      
+          user_name: ENV["SENDMAIL_USERNAME"],    
+          password: ENV["SENDMAIL_PASSWORD"],
+        }
 end
