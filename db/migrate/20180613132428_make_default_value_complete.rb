fab12716c114c2e3e3353b0b7b87c0e7d847e740
zgfif <zihzag@gmail.com>
2018-06-13 21:48:56 +0300

    modify gitignore

---
+++ db/migrate/20180613132428_make_default_value_complete.rb
@@ -0,0 +1,5 @@
+class MakeDefaultValueComplete < ActiveRecord::Migration[5.1]
+  def change
+  	change_column :events, :complete, :boolean, :default => false
+  end
+end
---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe EventsController, type: :controller do
+	context 'GET #index' do
+		it 'returns a success response' do
+			get :index
+			expect(response).to be_success # response.success?
+		end
+	end
+	context 'GET #show' do
+		it 'returns a success response'	do
+			event = Event.create!(title: 'Grumpy', tag: 'cat', scheduled_date: '2018-06-15' )
+			get :show, params: {id: event.to_param}
+			expect(response).to be_success
+		end		
+	end
+end
